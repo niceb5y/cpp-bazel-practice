9ec6775f021625760c80356e8e33c9317ddcf01f
Seungho Kim <niceb5y@gmail.com>
2019-10-29 18:56:29 +0900

    first commit

---
+++ main/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "math/add.h"
+
+int main(void) {
+  std::cout << "Hello World" << std::endl;
+  std::cout << 1 << " + " << 1 << " = " << add(1, 1) << std::endl;
+}
---
+++ tests/add.cpp
@@ -0,0 +1,4 @@
+#include "gtest/gtest.h"
+#include "math/add.h"
+
+TEST(Math, Add) { EXPECT_EQ(3, add(1, 2)); }
